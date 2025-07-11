3df005d1bb214ce4902d48b3540cda131ff8e191
masomi79 <masomi79@gmail.com>
2025-07-12 04:59:10 +0900

    front: register form

---
+++ miskitoapp-front/src/components/UserRegister.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="user-register">
+    <h2>sign up</h2>
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <label for="name">nombre</label>
+        <input id="name" v-model="name" required />
+      </div>
+      <div>
+        <label for="email">correo</label>
+        <input id="email" v-model="email" required />
+      </div>
+      <div>
+        <label for="password">contraseña</label>
+        <input id="passwowrd" v-model="password" required />
+      </div>
+      <button type="submit">registrar</button>
+    </form>
+    <p v-if="success">se ha registrado equistosamente!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WordRegister',
+  data() {
+    return {
+      name: '',
+      email: '',
+      password: '',
+      success: false,
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.success = true;
+      this.word = '';
+      this.meaning = '';
+    },
+  },
+};
+</script>
+
+<style scoped>
+.user-register {
+  max-width: 400px;
+  margin: 2rem auto;
+}
+</style>
--- miskitoapp-front/src/router/index.js
+++ miskitoapp-front/src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import WordSearch from '../components/WordSearch.vue'
 import WordDetail from '../components/WordDetail.vue'
 import AboutView from '../views/AboutView.vue'
+import UserRegisterView from '../views/UserRegisterView.vue'
 
 const routes = [
   { path: '/', component: WordSearch },
   { path: '/:lang/:id', component: WordDetail, props: true },
-  { path: '/about', name: 'About', component: AboutView, }
+  { path: '/about', name: 'About', component: AboutView, },
+  { path: '/register', component: UserRegisterView }
 ]
 
 const router = createRouter({
